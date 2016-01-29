b95c7474da4dbd9a79fec1ba555b0080795fda92
taysontey <taysontey@gmail.com>
2016-01-29 11:18:28 -0200

    Entity Mapping(2)

---
+++ Projeto.DAL/Mapping/JogadorMap.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using Projeto.Entity.Entities;
+
+namespace Projeto.DAL.Mapping
+{
+    public class JogadorMap : ClassMap<Jogador>
+    {
+        public JogadorMap()
+        {
+            Table("TB_JOGADOR");
+
+            Id(j => j.IdJogador, "IDJOGADOR")
+                .GeneratedBy.Identity();
+
+            Map(j => j.Nome, "NOME")
+                .Length(50)
+                .Not.Nullable();
+
+            Map(j => j.Apelido, "APELIDO")
+                .Length(50)
+                .Not.Nullable();
+
+            Map(j => j.DataNascimento, "DATANASCIMENTO")
+                .Not.Nullable();
+
+            Map(j => j.Posicao, "POSICAO")
+                .Length(25)
+                .Not.Nullable();
+
+            #region Relacionamentos
+
+            References(j => j.Time)
+                .Column("IDTIME")
+                .Not.LazyLoad()
+                .Not.Nullable();
+
+            #endregion
+        }
+    }
+}
---
+++ Projeto.DAL/Mapping/TimeMap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using Projeto.Entity.Entities;
+
+namespace Projeto.DAL.Mapping
+{
+    public class TimeMap : ClassMap<Time>
+    {
+        public TimeMap()
+        {
+            Table("TB_TIME");
+
+            Id(t => t.IdTime, "IDTIME")
+                .GeneratedBy.Identity();
+
+            Map(t => t.Nome, "NOME")
+                .Length(50)
+                .Not.Nullable();
+
+            Map(t => t.DataFundacao, "DATAFUNDACAO")
+                .Not.Nullable();
+
+            #region Relacionamentos
+
+            HasMany(t => t.Jogadores)
+                .KeyColumn("IDTIME")
+                .Inverse();
+
+            #endregion
+        }
+    }
+}
