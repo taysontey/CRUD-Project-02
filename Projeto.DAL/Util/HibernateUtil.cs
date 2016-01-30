dffd531813b0112bd4848a254286fa4fac24c820
taysontey <taysontey@gmail.com>
2016-01-30 13:15:32 -0200

    Entity Mapping(3)

---
+++ Projeto.DAL/Util/HibernateUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using FluentNHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using Projeto.DAL.Mapping;
+
+
+namespace Projeto.DAL.Util
+{
+    public class HibernateUtil
+    {
+        private static ISessionFactory factory;
+
+        public static ISessionFactory Factory
+        {
+            get
+            {
+                if (factory == null)
+                {
+                    factory = Fluently.Configure()
+                                      .Database(MsSqlConfiguration.MsSql2012.ConnectionString(ConfigurationManager.ConnectionStrings["projeto"].ConnectionString))
+                                      .Mappings(m => m.FluentMappings.AddFromAssemblyOf<TimeMap>())
+                                      .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+                                      .BuildSessionFactory();
+                }
+                return factory;
+            }
+        }
+    }
+}
